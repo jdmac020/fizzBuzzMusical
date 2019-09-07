0424f5babbf103ff3d38940b3b619b8688509419
jdmac020 <john.macgregor@gmail.com>
2019-09-07 09:58:38 -0400

    Moved FizzBuzzEngine to class field in tests

--- MusicalFizzBuzz/MusicalFizzBuzz.Library.Tests/FizzBuzzEngineShould.cs
+++ MusicalFizzBuzz/MusicalFizzBuzz.Library.Tests/FizzBuzzEngineShould.cs
@@ -1,6 +1,4 @@
-using System;
 using Xunit;
-using MusicalFizzBuzz.Library;
 using Shouldly;
 using System.Collections.Generic;
 
@@ -8,6 +6,8 @@ namespace MusicalFizzBuzz.Library.Tests
 {
     public class FizzBuzzEngineShould
     {
+        private FizzBuzzEngine _fizzBuzz = new FizzBuzzEngine();
+
         [Theory]
         [InlineData(2, "2")]
         [InlineData(8, "8")]
@@ -15,9 +15,7 @@ namespace MusicalFizzBuzz.Library.Tests
         public void ReturnNumberWhenNotSpecial(int testInput, 
             string expectedResult)
         {
-            var fizzBuzz = new FizzBuzzEngine();
-
-            var result = fizzBuzz.Process(testInput);
+            var result = _fizzBuzz.Process(testInput);
 
             result.ShouldBe(expectedResult);
         }
@@ -25,9 +23,7 @@ namespace MusicalFizzBuzz.Library.Tests
         [Fact]
         public void ReturnFizzWhenThree()
         {
-            var fizzBuzz = new FizzBuzzEngine();
-
-            var result = fizzBuzz.Process(3);
+            var result = _fizzBuzz.Process(3);
 
             result.ShouldBe("Fizz");
         }
@@ -36,12 +32,11 @@ namespace MusicalFizzBuzz.Library.Tests
         [InlineData(6, 9, 12, 18, 36, 303)]
         public void ReturnFizzWhenMultipleOfThree(params int[] testInputs)
         {
-            var fizzBuzz = new FizzBuzzEngine();
             var results = new List<string>();
 
             foreach (var input in testInputs)
             {
-                results.Add(fizzBuzz.Process(input));
+                results.Add(_fizzBuzz.Process(input));
             }
 
             results.ShouldAllBe(result => result.Equals("Fizz"));
@@ -50,9 +45,7 @@ namespace MusicalFizzBuzz.Library.Tests
         [Fact]
         public void ReturnBuzzWhenFive()
         {
-            var fizzBuzz = new FizzBuzzEngine();
-
-            var result = fizzBuzz.Process(5);
+            var result = _fizzBuzz.Process(5);
 
             result.ShouldBe("Buzz");
         }
@@ -61,37 +54,25 @@ namespace MusicalFizzBuzz.Library.Tests
         [InlineData(10,20,100,4000)]
         public void ReturnBuzzWhenMultipleOfFive(params int[] testInputs)
         {
-            var fizzBuzz = new FizzBuzzEngine();
             var results = new List<string>();
 
             foreach (var input in testInputs)
             {
-                results.Add(fizzBuzz.Process(input));
+                results.Add(_fizzBuzz.Process(input));
             }
 
             results.ShouldAllBe(result => result.Equals("Buzz"));
         }
 
-        [Fact]
-        public void ReturnFizzBuzzWhenFifteen()
-        {
-            var fizzBuzz = new FizzBuzzEngine();
-
-            var result = fizzBuzz.Process(15);
-
-            result.ShouldBe("FizzBuzz");
-        }
-
         [Theory]
-        [InlineData(30,45,300,1500)]
+        [InlineData(15,30,45,300,1500)]
         public void ReturnFizzBuzzForMultiplesOfThreeAndFive(params int[] testInputs)
         {
-            var fizzBuzz = new FizzBuzzEngine();
             var results = new List<string>();
 
             foreach (var input in testInputs)
             {
-                results.Add(fizzBuzz.Process(input));
+                results.Add(_fizzBuzz.Process(input));
             }
 
             results.ShouldAllBe(result => result.Equals("FizzBuzz"));
