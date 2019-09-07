8f5b925995d68681afcc7dd0a06710d71679e091
jdmac020 <john.macgregor@gmail.com>
2019-09-07 12:54:58 -0400

    Added console app and spike code for calling fizzbuzz

---
+++ MusicalFizzBuzz/MusicalFizzBuzz.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using MusicalFizzBuzz.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace MusicalFizzBuzz.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            var fizzBuzz = new FizzBuzzEngine();
+
+            var inputs = Enumerable.Range(0, 500);
+
+            foreach (var input in inputs)
+            {
+                var result = fizzBuzz.Process(input);
+
+                if (int.TryParse(result, out int intVar) == false)
+                {
+                    if (result == "FizzBuzz")
+                    {
+                        Console.WriteLine(result);
+                        Thread.Sleep(300);
+                    }
+                    if (result == "Buzz")
+                    {
+                        Console.Write($"{result} ");
+                        Thread.Sleep(250);
+                    }
+                    if (result == "Fizz")
+                    {
+                        Console.Write($"{result} ");
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
