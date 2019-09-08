43fa80d81d25a1d4b43a15a4f6f72935f13287a2
jdmac020 <john.macgregor@gmail.com>
2019-09-08 08:45:00 -0400

    SongPlayer returns fixed string

---
+++ MusicalFizzBuzz/MusicalFizzBuzz.Library.Tests/SongPlayerShould.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using Shouldly;
+
+namespace MusicalFizzBuzz.Library.Tests
+{
+    public class SongPlayerShould
+    {
+        [Fact]
+        public void ConvertIntArrayToSpaceDelimitedString()
+        {
+            var testArray = new int[]
+            {
+                3,5,15
+            };
+            var expectedReturn = "Fizz Buzz FizzBuzz";
+
+            var songPlayer = new SongPlayer();
+
+            var result = songPlayer.Play(testArray);
+
+            result.ShouldBe(expectedReturn);
+        }
+    }
+}
---
+++ MusicalFizzBuzz/MusicalFizzBuzz.Library/SongPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicalFizzBuzz.Library
+{
+    public class SongPlayer
+    {
+        public object Play(int[] testArray)
+        {
+            return "Fizz Buzz FizzBuzz";
+        }
+    }
+}
